fba1870c85c5b907b3ee764fdd79334d49660aed
thanhluann <imthanhluan@gmail.com>
2022-06-26 22:06:58 +0700

    add new img

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ const hamburger = document.querySelector('.menu-btn__burger');
 const mynav = document.querySelector('.mynav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
+const caraImg = document.querySelector('#caraImg')
+const ntlImg = document.querySelector('#ntlImg')
+const portfolioImg = document.querySelector('#portfolioImg')
 
 let showMenu = false;
 
@@ -25,3 +28,68 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+function slideShowCaraImg(){
+  let i = 0;
+  setInterval(function(){
+  caraImg.src = caraImgList[i]
+
+  i++
+  if(i >= caraImgList.length-1){
+    i = 0
+  }
+  }, 1300)
+}
+
+function slideShowNtlShopImg(){
+  let i = 0;
+  setInterval(function(){
+  ntlImg.src = ntlImgList[i]
+  i++
+  if(i >= ntlImgList.length-1){
+    i = 0
+  }
+  }, 1500)
+}
+
+function slideShowPortfolioImg(){
+  let i = 0;
+  setInterval(function(){
+    portfolioImg.src = portfolioImgList[i]
+  i++
+  if(i >= portfolioImgList.length-1){
+    i = 0
+  }
+  }, 1700)
+}
+
+const caraImgList = [
+  '../assets/img/caraAdminDashbar.png',
+  '../assets/img/caraMen.png',
+  '../assets/img/caraClientHome.png',
+  '../assets/img/caraProductDetail.png',
+  '../assets/img/caraProductManager.png',
+  '../assets/img/caraWomen.png',
+]
+
+const ntlImgList = [
+  '../assets/img/ntlShop.png',
+  '../assets/img/ntlShop2.png',
+  '../assets/img/ntlShopLogin.png',
+  '../assets/img/ntlShopRegister.png',
+]
+
+const portfolioImgList = [
+  '../assets/img/portfolioAbout.png',
+  '../assets/img/portfolioContact.png',
+  '../assets/img/portfolioHome.png',
+  '../assets/img/portfolioProjects.png',
+]
+
+
+
+slideShowCaraImg();
+
+slideShowNtlShopImg();
+
+slideShowPortfolioImg()
